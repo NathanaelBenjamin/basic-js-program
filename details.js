64b68e3b093204c173f07c230f4369282f8a8ce6
Nathanael Benjamin <oluwakayodenathanael@gmail.com>
2022-06-23 14:59:15 +0100

    Add script

---
+++ details.js
@@ -0,0 +1,9 @@
+function printDetails(){
+    let name, height, country;
+    name = 'Benjamin Oluwakayode Nathanael';
+    height = '5 feet and 9 inches.'
+    country = 'Nigeria';
+    return `name: ${name}, height: ${height} and country: ${country}`;
+}
+
+console.log(printDetails());
